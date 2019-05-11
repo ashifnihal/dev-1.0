130d9b511768597cbb29c71cae0ed318bc9f4edb
ashifnihal <38804341+ashifnihal@users.noreply.github.com>
2019-05-11 18:22:43 +0530

    Revert "Dev 1.3"

--- SMS_PROJ1/testapp/sms_api1/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from testapp.models import SMSPackageDetails
-class SMS_Package_ModelForm(forms.ModelForm):
-    class Meta:
-        model=SMSPackageDetails
-        fields='__all__'
